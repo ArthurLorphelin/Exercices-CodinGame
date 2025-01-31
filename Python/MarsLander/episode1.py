eb4a0abb9abef7677e48d118019cb4382a325a7c
ArthurLorphelin <arthur.lorphelin@yahoo.fr>
2025-01-31 12:05:04 +0100

    Refacto Mars Lander Episode 1

--- Python/MarsLander/episode1.py
+++ Python/MarsLander/episode1.py
@@ -1,7 +1,7 @@
 import sys
 
+# Fonction qui calcule la puissance des fusées en fonction de la vitesse verticale
 def compute_power(v_speed):
-    """Calcule la puissance des fusées en fonction de la vitesse verticale."""
     if v_speed < -30:
         return 4
     elif v_speed < -20:
@@ -11,26 +11,25 @@ def compute_power(v_speed):
     else:
         return 0
 
-def main():
-    # Lecture des points de la surface de Mars
-    surface_n = int(input())  # Nombre de points formant la surface
-    surface = []
-    for _ in range(surface_n):
-        land_x, land_y = map(int, input().split())
-        surface.append((land_x, land_y))  # Stockage des coordonnées de la surface
+# Fonction qui lit et stocke les points de la surface de Mars
+def read_surface():
+    surface_n = int(input())
+    return [tuple(map(int, input().split())) for _ in range(surface_n)]
 
-    # Boucle du jeu
+# Boucle principale du jeu
+def control_lander():
+    surface = read_surface()
+
     while True:
-        x, y, h_speed, v_speed, fuel, rotate, power = map(int, input().split())
-
-        # Calcul de la puissance nécessaire pour ralentir la descente
+        _, _, _, v_speed, _, _, _ = map(int, input().split())
         power_needed = compute_power(v_speed)
 
-        # La solution est toujours un angle de 0 (pas de rotation nécessaire pour le niveau 1)
-        solution = f"0 {power_needed}"
+        # On affiche la solution
+        print(f"0 {power_needed}")
 
-        # Affichage de la solution
-        print(solution)
+def main():
+   control_lander()
+
 
 if __name__ == "__main__":
     main()
