e26cd79e9d40f091a41b5f6bc552f5a16f21dd30
ArthurLorphelin <arthur.lorphelin@yahoo.fr>
2025-01-31 14:43:01 +0100

    Refacto Temperatures

--- Python/Temperatures/temperatures.py
+++ Python/Temperatures/temperatures.py
@@ -1,19 +1,26 @@
-import sys
+# Fonction qui lit les températures et les retourne dans une liste
+def get_temperature_list(n):
+    return list(map(int, input().split())) if n > 0 else []
+
+
+# Fonction qui trouve la température la plus proche de zéro
+def find_closest_temperature(temperature_list):
+    if not temperature_list:
+        return 0
+
+    temp_closest_to_zero = temperature_list[0]
+    for temp in temperature_list:
+        if (abs(temp) < abs(temp_closest_to_zero)) or (
+                abs(temp) == abs(temp_closest_to_zero) and temp > temp_closest_to_zero):
+            temp_closest_to_zero = temp
+
+    return temp_closest_to_zero
+
 
 def main():
     n = int(input().strip())
-    temperature_list = list(map(int, input().split())) if n > 0 else []
-
-    # On vérifie si la liste est vide
-    if not temperature_list:
-        print(0)
-        return
-    
-    # On trouve la témpérature la plus proche de 0
-    temp_closest_to_zero = temperature_list[0]
-    for temp in temperature_list:
-        if (abs(temp) < abs(temp_closest_to_zero)) or (abs(temp) == abs(temp_closest_to_zero) and temp > temp_closest_to_zero):
-            temp_closest_to_zero = temp
+    temperature_list = get_temperature_list(n)
+    temp_closest_to_zero = find_closest_temperature(temperature_list)
 
     # On affiche le résultat
     print(temp_closest_to_zero)
