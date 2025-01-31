3753fef41966c813f1f6788a471d0eb53a5ae49a
ArthurLorphelin <arthur.lorphelin@yahoo.fr>
2025-01-31 14:29:11 +0100

    Refacto Power of Thor Episode 1

--- Python/PowerOfThor/episode1.py
+++ Python/PowerOfThor/episode1.py
@@ -1,32 +1,40 @@
-import sys
-
-def main():
+# Fonction qui lit et retourne les positions initiales de l'éclair et de Thor
+def get_initial_positions():
     light_x, light_y, thor_x, thor_y = map(int, input().split())
+    return light_x, light_y, thor_x, thor_y
 
-    # Boucle du jeu
+# Fonction qui détermine le mouvement de Thor vers l'éclair et retourne la direction
+def move_thor(thor_x, thor_y, light_x, light_y):
+    direction = ""
+
+    # Déplacement vertical
+    if thor_y > light_y:
+        thor_y -= 1
+        direction += "N"
+    elif thor_y < light_y:
+        thor_y += 1
+        direction += "S"
+
+    # Déplacement horizontal
+    if thor_x > light_x:
+        thor_x -= 1
+        direction += "W"
+    elif thor_x < light_x:
+        thor_x += 1
+        direction += "E"
+
+    return thor_x, thor_y, direction
+
+# Boucle principale du jeu
+def main():
+    light_x, light_y, thor_x, thor_y = get_initial_positions()
+
     while True:
         remaining_turns = int(input())
-        direction = ""
+        thor_x, thor_y, direction = move_thor(thor_x, thor_y, light_x, light_y)
 
-        # On détermine si la direction est verticale
-        if thor_y > light_y:
-            thor_y -= 1
-            direction += "N"
-        elif thor_y < light_y:
-            thor_y += 1
-            direction += "S"
-
-        # On détermine si la direction est horizontale
-        if thor_x > light_x:
-            thor_x -= 1
-            direction += "W"
-        elif thor_x < light_x:
-            thor_x += 1
-            direction += "E"
-
-        # On affiche la direction
+        # On affiche la solution
         print(direction)
-
 
 if __name__ == "__main__":
     main()
